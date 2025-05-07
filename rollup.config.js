ff75be46e848b42846177049bb11b34f5b77553b
elipeF <matej6543211@gmail.com>
2025-05-07 19:27:26 +0200

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json' assert { type: 'json' };
+
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+export default [
+  // ESM build
+  {
+    input: 'src/index.ts',
+    external,
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: false,
+        compilerOptions: { outDir: undefined }
+      })
+    ],
+    output: {
+      dir: 'dist/esm',
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].js'
+    }
+  },
+
+  // CJS build
+  {
+    input: 'src/index.ts',
+    external,
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: false,
+        compilerOptions: { outDir: undefined }
+      })
+    ],
+    output: {
+      dir: 'dist/cjs',
+      format: 'cjs',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].js'
+    }
+  }
+];
